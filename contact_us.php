252072c1af44903763381fd7e32aedf8b0379ca2
Abhishek Singh <abhi.singh.solanki@gmail.com>
2020-09-20 18:26:27 +0530

    A working website files along with CSS HTML

---
+++ contact_us.php
@@ -0,0 +1,188 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html><head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-19799504-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-19799504-2');
+</script>
+<link rel="shortcut icon" href="favicon.ico">
+<link rel="icon" type="image/png" href="http://www.solanki.co.in/images/favicon.png">
+<title>Careertree :: Good learning!</title>
+<meta name="DevelopedDesignedby" content="Solanki Infoway www.soiway.com">
+<meta content="General" name="rating">
+<meta content="document" name="resource-type">
+<meta content="global" name="distribution">
+<meta name="robots" content="index,follow">
+<meta name="ROBOTS" content="NOODP">
+<meta NAME="robots" CONTENT="index, follow">
+<meta name="author" content="Solanki Infoway Pvt. Ltd." />
+<meta name="description" content="Solanki Technologies is an innovation, technology and services company focused exclusively on Information Technology. Welcome to the 'soiway' home page, entry point to information about innovative soiway products, business solutions, Web Strategy Consulting, Design and Development, Online Branding, Usability & User Experience Management, Web Hosting, Domain Registration, Search Engine Optimisation & Marketing, Social Media Marketing and Visual design." />
+<meta name="keywords" content="website design patna, patna web hosting, web design bihar, Open source customization, Web application, SEO Company Patna, Internet marketing Patna, Patna Web design, Website in Patna, Domain registration, Patna web services, website design Hajipur, website development Hajipur Bihar, Software Hajipur, Wesite hosting Hajipur, website design Muzaffarpur, website development Muzaffarpur Bihar, Software Muzaffarpur, Website hosting Muzaffarpur, Website design India, Ecommerce website design, online logo design, Graphic design website, soiway, solanki technologies, solanki india, Solanki Infoway Private Limited, Website design company, Custom website design, Affordable website design, Cheap website design, web design logo, custom logo design, Search engine optimization in India, PPC Management in India, SEO Services India, Cheap logo design, Web design logo, Graphic design logo, Logo design India, soiway-patna, custom website development, Web design company India, ecommerce website development, website design development, website development India, Business card design online, Brand identity design, custom web development, ecommerce web development, web development company, ecommerce web site design, affordable web site design, custom web site design, search engine submission, solanki, solanki infoway, internet marketing, logo design, web development, multimedia CD presentation, web design, multimedia presentation, web marketing, e-commerce site, flash design, web designing, shopping cart, web page design, software programming, online programming, website graphics, web design company, Soiway Hajipur, web development USA, search engine optimization UK, web design Spain, multimedia CD presentaion new zealand, internet marketing UAE, No. 1 Web Design Company of Bihar, search engine marketing canada, website design at Rs. 2500, website design at Rs. 5000, 2 Gb hosting Rs. 999., Indian company for website designing, Website design Patna, Website Design at Rs 10000, Web design &amp; hosting Patna, domain registration patna, Hajipur, Motihari, Behiah, Bhagalpur, Begusarai, Chhapra, Chapra, Siwan, Gopalganj, vaishali, The leading website design and development brand in Bihar and North-East, Solanki Infoway Pvt. Ltd., India" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta http-equiv="Expires" content="0">
+<link rel="stylesheet" type="text/css" href="files/soiway.css">
+</style><style type="text/css">
+<!--
+body {
+	background-image: url(files/bg1.jpg);
+}
+-->
+</style>
+</head><body scroll="yes">
+<div></div>
+	
+	<div id="MasterPageMainContent">
+		<div id="MasterPageHeader">
+			<div id="SiteImage">
+				<p><a href="http://www.careertree.in"><img src="files/careertreelogo.png" alt="CareerTree" width="199" height="70" /></a><a href="http://www.solanki.co.in"></a></p>
+</div>
+				<div id="LoginAndSearch">
+					<div id="Login">
+						<!-- Begin Global Links (sign in and contact us -->
+								
+							<table>
+								<tbody>
+								  <div><tr>
+									<td valign="middle" ><a href="index.html" class="ms-formdescription">Home</a></td>
+							    <td style="padding-left: 1px; padding-right: 3px;" >|</td>
+					<td valign="middle" ><a href="contact_us.php" class="ms-formdescription">Contact us</a>
+</td>
+								</tr></div>
+				  			</tbody></table>
+		  				
+					</div>
+		  </div>
+				<!-- End Logo and Search -->
+			</div>
+		<div id="TopNavDiv">
+		  <div>
+		    <table id="zz1_CustomMenu" class="ms-topNavContainer zz1_CustomMenu_2" border="0" cellpadding="0" cellspacing="0">
+		      <tbody>
+		        <tr>
+		          <td onmouseover="Menu_HoverStatic(this)" onmouseout="Menu_Unhover(this)" onkeyup="Menu_Key(event)" id="zz1_CustomMenun0"><table class="ms-topnav zz1_CustomMenu_4" border="0" cellpadding="0" cellspacing="0" width="100%">
+		            <tbody>
+		              <tr>
+		                <td style="white-space: nowrap;"><a href="index.html" style="border-style: none; font-size: 1em;"><span class="home-page-tiles">Home</span></a></td>
+		                <td style="width: 0pt;"><img src="files/menudark.gif" alt="" style="border-style: none; vertical-align: middle;" /></td>
+	                  </tr>
+	                </tbody>
+		            </table></td>
+		          <td style="width: 0px;"></td>
+		          <td style="width: 0px;"></td>
+		          <td onmouseover="Menu_HoverStatic(this)" onmouseout="Menu_Unhover(this)" onkeyup="Menu_Key(event)" id="zz1_CustomMenun1"><table class="ms-topnav zz1_CustomMenu_4" border="0" cellpadding="0" cellspacing="0" width="100%">
+		            <tbody>
+		              <tr>
+		                <td style="white-space: nowrap;"><a class="zz1_CustomMenu_1 ms-topnav zz1_CustomMenu_3" href="about_us.html" style="border-style: none; font-size: 1em;">about us</a></td>
+		                <td style="width: 0pt;"><img src="files/menudark.gif" alt="" style="border-style: none; vertical-align: middle;" /></td>
+	                  </tr>
+	                </tbody>
+		            </table></td>
+		          <td style="width: 0px;"></td>
+		          <td style="width: 0px;"></td>
+		          <td onmouseover="Menu_HoverStatic(this)" onmouseout="Menu_Unhover(this)" onkeyup="Menu_Key(event)" id="zz1_CustomMenun2"><table class="ms-topnav zz1_CustomMenu_4" border="0" cellpadding="0" cellspacing="0" width="100%">
+		            <tbody>
+		              <tr>
+		                <td style="white-space: nowrap;"><a class="zz1_CustomMenu_1 ms-topnav zz1_CustomMenu_3" href="software.html" style="border-style: none; font-size: 1em;">Software</a></td>
+		                <td style="width: 0pt;"><img src="files/menudark.gif" alt="" style="border-style: none; vertical-align: middle;" /></td>
+	                  </tr>
+	                </tbody>
+		            </table></td>
+		          <td style="width: 0px;"></td>
+		          <td style="width: 0px;"></td>
+		          <td onmouseover="Menu_HoverStatic(this)" onmouseout="Menu_Unhover(this)" onkeyup="Menu_Key(event)" id="zz1_CustomMenun3"><table class="ms-topnav zz1_CustomMenu_4" border="0" cellpadding="0" cellspacing="0" width="100%">
+		            <tbody>
+		              <tr>
+		                <td style="white-space: nowrap;"><a class="zz1_CustomMenu_1 ms-topnav zz1_CustomMenu_3" href="science.html" style="border-style: none; font-size: 1em;">Science</a></td>
+		                <td style="width: 0pt;"><img src="files/menudark.gif" alt="" style="border-style: none; vertical-align: middle;" /></td>
+	                  </tr>
+	                </tbody>
+		            </table></td>
+		          <td style="width: 0px;"></td>
+		          <td style="width: 0px;"></td>
+		          <td onmouseover="Menu_HoverStatic(this)" onmouseout="Menu_Unhover(this)" onkeyup="Menu_Key(event)" id="zz1_CustomMenun4"><table class="ms-topnav zz1_CustomMenu_4" border="0" cellpadding="0" cellspacing="0" width="100%">
+		            <tbody>
+		              <tr>
+		                <td style="white-space: nowrap;"><a class="zz1_CustomMenu_1 ms-topnav zz1_CustomMenu_3" href="technology.html" style="border-style: none; font-size: 1em;">Technology</a></td>
+		                <td style="width: 0pt;"><img src="files/menudark.gif" alt="" style="border-style: none; vertical-align: middle;" /></td>
+	                  </tr>
+	                </tbody>
+		            </table></td>
+		          <td style="width: 0px;"></td>
+		          <td style="width: 0px;"></td>
+		          <td onmouseover="Menu_HoverStatic(this)" onmouseout="Menu_Unhover(this)" onkeyup="Menu_Key(event)" id="zz1_CustomMenun5"><table class="ms-topnav zz1_CustomMenu_4" border="0" cellpadding="0" cellspacing="0" width="100%">
+		            <tbody>
+		              <tr>
+		                <td style="white-space: nowrap;"><a class="zz1_CustomMenu_1 ms-topnav zz1_CustomMenu_3" href="management.html" style="border-style: none; font-size: 1em;">Management</a></td>
+	                  </tr>
+	                </tbody>
+		            </table></td>
+		          <td style="width: 0px;"></td>
+		          <td style="width: 0px;"></td>
+		          <td onmouseover="Menu_HoverStatic(this)" onmouseout="Menu_Unhover(this)" onkeyup="Menu_Key(event)" id="zz1_CustomMenun6"><table class="ms-topnav zz1_CustomMenu_4" border="0" cellpadding="0" cellspacing="0" width="100%">
+		            <tbody>
+		              <tr>
+		                <td style="white-space: nowrap;"><a class="zz1_CustomMenu_1 ms-topnav zz1_CustomMenu_3" href="contact_us.php" style="border-style: none; font-size: 1em;">contact</a></td>
+	                  </tr>
+	                </tbody>
+		            </table></td>
+		          <td style="width: 0px;"></td>
+	            </tr>
+	          </tbody>
+	        </table>
+	      </div>
+	  </div>
+		<div id="PlaceHolderMainDiv">
+  <div>
+<!--BEGIN MAIN CONTENT DIV-->
+<div style="background-color: white;" class="gray-side-borders page-header">
+<div class="home-page-tiles">
+<table toplevel="" border="0" cellpadding="0" cellspacing="0" width="100%">
+	<tbody><tr>
+		<td valign="top"><img src="files/aboutus.jpg" width="958" height="200" alt="Careertree" /></td>
+	</tr>
+</tbody></table>
+</div>
+<!-- END HOME PAGE TILES -->
+</div>
+<div class="gray-side-borders content-below-page-header">
+<div style="height: 26px; width: 100%; background-repeat: repeat-x;">
+</div>
+<div style="width: 900px; margin: 0pt auto;">
+  <div class="gray-side-borders home-page-main" style="max-width: 938px; height: inherit;">
+    <h1>Contact </h1>
+	<p style="color: rgb(204, 0, 51); font-weight: bold;">Address:</p>
+	<p class="ms-formdescriptionnew" style="color: #666; font-weight: normal;"><strong></strong># 1602-A, Top Floor (Left Side) </p>
+	<p class="ms-formdescriptionnew" style="color: #666; font-weight: normal;">Str. No. 13, Near Valmiki Chowk</p>
+	<p class="ms-formdescriptionnew" style="color: #666; font-weight: normal;">Govindpuri, Kalkaji, New Delhi - 110019, India</p>
+	<p class="ms-formdescriptionnew" style="color: #666; font-weight: normal;">E-mail: abhi.singh.solanki@gmail.com (p) and info@careertree.in</p>
+<p class="ms-formdescriptionnew" style="color: #666; font-weight: normal;">One may also visit my astro prediction site &quot;Astro.Sys&quot; http://www.abhisheksingh.in</p>
+<div class="divRSS">
+<p class="ms-formdescriptionnew" style="color: #666; font-weight: normal;">Wordpress: http://en.gravatar.com/abhisinghsolanki</p>
+  <div style="padding-left: 10px; margin-top: 5px; vertical-align: bottom; color: #CC0033;"> <a href="http://www.solanki.co.in/#">Social Profile: </a></div>
+</div>
+<div class="divRSS">
+  <div style="padding-left: 10px; margin-top: 5px; vertical-align: bottom; color: #CC0033;"><a href="https://www.facebook.com/abhisheksingh.asia"><span class="ms-formdescriptionnew" style="color: #666; font-weight: bold;"><img src="files/facebook.png" width="32" height="32" alt="FB" /></span></a><a href="https://www.twitter.com/UIabhisheksingh"><img src="files/twitter.png" width="32" height="32" /></a><a href="https://www.linkedin.com/in/abhi15"><img src="files/linkedIn-presence.png" width="32" height="32" /></a></div>
+</div>
+<p class="ms-formdescriptionnew" style="color: #666; font-weight: normal;">&nbsp;</p>
+<p>This web application is powered by <a href="http://www.solanki.co.in">Solanki Technologies</a></p>
+  </div>
+		</div>
+	</div>
+</div>
+
+
+
+					<div id="footer">
+					<div id="privacypolicy">
+					<a href="PrivacyPolicy.html">Privacy Policy</a> | <a href="TermsOfUse.html">Terms of Use</a> |  <a href="SiteIndex.html">Site Index</a> 
+					</div>
+					<div id="copyright"> © 2020 careertree.in  
+					  All Rights Reserved.</div>
+					</div>
+				<div id="graybottom"></div>
+</div>
+</div>
+</body></html>
